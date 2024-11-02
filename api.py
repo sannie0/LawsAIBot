837914cb6ccdd65a7679ac4b1b62bf7eb9f9e0be
Sannie <Sannie>
2024-11-02 23:45:49 +0500

    removed environment variables

--- api.py
+++ api.py
@@ -1,5 +1,4 @@
 import requests
-
 promt = {
     "modelUri": "gpt://b1gtm1dktbnprji5rmcp/yandexgpt-lite",
     #"modelUri": "gpt://b1gtm1dktbnprji5rmcp/gpt-model-id",
@@ -15,27 +14,12 @@ promt = {
         }
      ]
 }
-
-
-'''!!!создать переменную среды через cmd для ключа - set API_KEY=наш_ключ
-создать переменную среды через cmd для ключа - set FOLDER_ID=ваш_folder_id
-import os
-
-# Получение API-ключа из переменной среды
-api_key = os.getenv("API_KEY")
-
-#Получение id из переменной среды
-folder_id = os.getenv("FOLDER_ID")
-
-'''
-
 url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
 headers = {
     "Content-Type": "application/json",
-    "Authorization": f"Api-Key {api_key}",
-    "x-folder-id": folder_id
+    "Authorization": "Api-Key AQVNxzCOvKxKywDPo_DjEsDADV_40iHXi-_-7aID",
+    "x-folder-id": "b1gtm1dktbnprji5rmcp"
 }
-
 response = requests.post(url, headers=headers, json=promt)
 result = response.text
 print(result)
